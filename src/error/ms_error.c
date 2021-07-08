a01c823853c4477e43430b5158cf418299c49568
sshakya <sshakya@student.42.fr>
2021-07-09 01:18:22 +0200

    :construction: Added more error return functions
    *
    
    void ms_errmsg(int id, char *str)
    	- use to print custom message
    
    void ms_error_token(t_tlist *tlist, int id)
    	- use to exit during token creation
    
    void ms_error(t_ms *data, int id)
    	- use to exit from main
    
    *

--- src/error/ms_error.c
+++ src/error/ms_error.c
@@ -6,21 +6,35 @@
 /*   By: sshakya <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/08 14:51:16 by sshakya           #+#    #+#             */
-/*   Updated: 2021/07/08 19:13:56 by sshakya          ###   ########.fr       */
+/*   Updated: 2021/07/09 01:17:43 by sshakya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ms_minishell.h"
 
-void	ms_error_token(t_tlist *tlist, int errnum)
+/*
+** NEED SINGLE PROTOTYPE TO EXIT DURING LEXER/PARSER/OTHER !!!!!!!
+*/
+static void	ms_errmsg(int id, char *str)
 {
-	(void)errnum;
-	(void)tlist;
+	if (id == ERR_SYN)
+		ft_putstr_fd(2, "syntax error near unexpected token");
+	if (id == ERR_TK)
+		ft_putstr_fd(2, "error message for ERR_TK");
+	if (str)
+		ft_putstr_fd(2, str);
 }
 
-void	ms_error(t_ms *data, int type)
+void	ms_error_token(t_tlist *tlist, int id)
 {
-	if (data->tlist)
-		ms_clean(data);
+	(void)tlist;
+	if (id == ERR_SYN)
+		ms_errmsg(ERR_SYN, "'|'");
+	return ;
+}
+
+void	ms_error(t_ms *data, int id)
+{
+	ms_clean(data);
 	return ;
 }
