cc76b2266e237bf0d1f895011df12acff496df73
Satcheen SHAKYA <Shakira@dump-accueil-1.42.fr>
2021-07-20 12:09:59 +0200

    :construction: Split ms_parser into small fn
    *
    
    Improve readbility
    
    *

--- src/parser/ms_parser.c
+++ src/parser/ms_parser.c
@@ -6,7 +6,7 @@
 /*   By: atruphem <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/08 10:52:09 by atruphem          #+#    #+#             */
-/*   Updated: 2021/07/19 21:05:04 by toni             ###   ########.fr       */
+/*   Updated: 2021/07/20 12:09:48 by sshakya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,7 +45,7 @@ int		ms_count_args(t_tlist *tlist)
 {
 	t_tlist	*current;
 	int		i;
-	
+
 	i = 0;
 	current = tlist;
 	while (current && current->tk.type == WORD)
@@ -56,95 +56,108 @@ int		ms_count_args(t_tlist *tlist)
 	return (i);
 }
 
+int	ms_init_parser(t_node **node, t_command **command)
+{
+	*node = malloc(sizeof(t_node));
+	if (*node == NULL)
+		return (NULL);
+	*command = malloc(sizeof(t_command));
+	if (*command == NULL)
+	{
+		free(*node);
+		return (NULL);
+	}
+	node->type = COMMAND;
+	node->left = NULL;
+	node->right = NULL;
+	node->data = new_command;
+	command->cmd = NULL;
+	command->buildin = 0;
+	command->args = NULL;
+	command->redirIN = 0;
+	command->redirOUT = 0;
+	command->INfd = 0;
+	command->delimiter = NULL;
+	command->OUTfd = 1;
+}
+
+static int	ms_redir_in(t_tlist **token, t_node *node, t_command *command)
+{
+	char *file;
+
+	file = NULL;
+	if ((*token)->next->tk.type != WORD)
+		return (ERR_SYN);
+	command->redirIn = 1;
+	file = ms_format_file((*token)->next->tk.value);
+	command->INfd = open(file, O_RDONLY);
+	free(file);
+	if (command->INfd < 0)
+		return (ERR_OPEN);
+	*token = (*token)->next;
+}
+
+static int	ms_redir_out(t_tlist **token, t_node *node, t_command *command)
+{
+	char *file;
+
+	file = NULL;
+	if ((*token)->next->tk.type != WORD)
+		return (ERR_SYN);
+	command->redirOUT = 1;
+	file = ms_format_file((*token)->next->tk.value);
+	new_command->INfd = open(file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	free(file);
+	if (INfd < 0)
+		return (ERR_OPEN);
+	*token = (*token)->next->next;
+}
+
+static int	ms_redir_outa(t_tlist **token, t_node *node, t_command *command)
+{
+	char *file;
+
+	file = NULL;
+	if ((*token)->next->tk.type != WORD)
+		return (ERR_SYN);
+	command->redirOUT = 1;
+	file = ms_format_file(current->next->tk.value);
+	command->INfd = open(file, O_WRONLY | O_CREAT | O_APPEND, 0644);
+	free(file);
+	if (INfd < 0)
+		return (ERR_OPEN);
+	*token = (*token)->next->next;
+}
+
 t_node	*create_command(t_tlist *tlist)
 {
 	t_node		*new_node;
 	t_command	*new_command;
-	char		*file;
 	int		buildin;
 	t_tlist	*current;
 	int		i;
 
-	new_node = malloc(sizeof(t_node));
-	if (!new_node)
-		return (NULL);
-	new_command = malloc(sizeof(t_command));
-	if (!new_command)
-	{
-		free(new_node);
-		return (NULL);
-	}
-	new_node->type = COMMAND;
-	new_node->left = NULL;
-	new_node->right = NULL;
-	new_node->data = new_command;
-	new_command->cmd = NULL;
-	new_command->buildin = 0;
-	new_command->args = NULL;
-	new_command->redirIN = 0;
-	new_command->redirOUT = 0;
-	new_command->INfd = 0;
-	new_command->delimiter = NULL;
-	new_command->OUTfd = 1;
+	ms_init_parser(&new_node, &new_command);
 	current = tlist;
-	while (current && current->tk.type != OP_PIPE && current->tk.type != OP_AND && current->tk.type != OP_OR)
+	while (current && current->tk.type != OP_PIPE
+			&& current->tk.type != OP_AND
+			&& current->tk.type != OP_OR)
 	{
 		if (current->tk.type == REDIR_IN)
-		{
-			if (current->next->tk.type != WORD)
-				return (error_syntax_NULL(current->next, &new_node));
-			new_command->redirIN = 1;
-			file = ms_format_file(current->next->tk.value);
-			new_command->INfd = open(file, O_RDONLY);
-			free(file);
-			if (INfd < 0)
-			{
-				free(new_command);
-				free(new_node);
-				return (error_open(current->next));
-			}		
-			current = current->next->next;
-		}
+			ms_redir_in(&current, new_node, new_command);
 		if (current->tk.type == REDIR_OUT)
-		{
-			if (current->next->tk.type != WORD)
-				return (error_syntax_NULL(current->next, &new_node));
-			new_command->redirOUT = 1;
-			file = ms_format_file(current->next->tk.value);
-			new_command->INfd = open(file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-			free(file);
-			if (INfd < 0)
-			{
-				free(new_command);
-				free(new_node);
-				return (error_open(current->next));
-			}		
-			current = current->next->next;
-		}
+			ms_redir_out(&current, new_node, new_command);
 		if (current->tk.type == REDIR_OUT_A)
-		{
-			if (current->next->tk.type != WORD)
-				return (error_syntax_NULL(current->next, &new_node));
-			new_command->redirOUT = 1;
-			file = ms_format_file(current->next->tk.value);
-			new_command->INfd = open(file, O_WRONLY | O_CREAT | O_APPEND, 0644);
-			free(file);
-			if (INfd < 0)
-			{
-				free(new_command);
-				free(new_node);
-				return (error_open(current->next));
-			}		
-			current = current->next->next;
-		}
-	//	if (tlist->tk.type == REDIR_IN_A)
-	//	if (tlist->tk.type == REDIR_IN_A)
-	//	if (tlist->tk.type == REDIR_IN_A)
-	//	{
-	//		if (tlist->next->tk.type != WORD)
-	//			return (error_syntax_NULL(tlist->next, &new_node));
-	//		new_command->redirIN = 2;
-	//		new_command->delimiter = tlist->next->
+			ms_redir_outa(&current, new_node, new_command);
+		//	
+		//	if (tlist->tk.type == REDIR_IN_A)
+		//	if (tlist->tk.type == REDIR_IN_A)
+		//	if (tlist->tk.type == REDIR_IN_A)
+		//	{
+		//		if (tlist->next->tk.type != WORD)
+		//			return (error_syntax_NULL(tlist->next, &new_node));
+		//		new_command->redirIN = 2;
+		//		new_command->delimiter = tlist->next->
 		if (current->tk.type == WORD)
 		{
 			if (new_command->cmd == NULL)
@@ -168,22 +181,26 @@ t_node	*create_command(t_tlist *tlist)
 				}
 			}
 		}
-int	ms_parser(t_ms *data)
+	}
+	}
+}
 
+int	ms_parser(t_ms *data)
+{
 	t_tlist		*current;
 	int			count;
 	t_node		*head;
 
 	current = data->tlist;
 	if (current.tk.type != WORD && current.tk.type != REDIR_IN 
-		&& current.tk.type != REDIR_OUT && current.tk.type != REDIR_IN_A
-		&& current.tk.type != REDIR_OUT_A)
+			&& current.tk.type != REDIR_OUT && current.tk.type != REDIR_IN_A
+			&& current.tk.type != REDIR_OUT_A)
 		return (error_syntax(current));
 	count = 0;
 	while (current)
 	{
 		if (current.tk.type == OP_PIPE || current.tk.type == OP_AND
-			|| current.tk.type == OP_OR)
+				|| current.tk.type == OP_OR)
 			count++;
 		current = current->next;
 	}
@@ -192,3 +209,5 @@ int	ms_parser(t_ms *data)
 	else
 	{
 		head = create_tree(data, count);
+	}
+}
