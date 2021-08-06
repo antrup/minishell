3b8108b3f5dcfb4269c405ecb57d2cf7aa5937c9
Satcheen Shakya <sshakya@student.42.fr>
2021-08-06 15:08:12 +0200

    :bug: wildcard fix for hidden files

--- src/expanser/ms_wildcard_matches.c
+++ src/expanser/ms_wildcard_matches.c
@@ -12,13 +12,15 @@
 
 #include "ms_minishell.h"
 
-static int	ms_ismatch(t_wcard *files, t_wcard *wcard)
+static int	ms_ismatch(t_wcard *files, t_wcard *wcard, int show_hidden)
 {
 	char	*temp;
 	int		flag;
 
 	temp = files->str;
 	flag = 0;
+	if (!show_hidden && files->str[0] == '.')
+		return (0);
 	while (temp && temp[0] != '\0')
 	{	
 		if (!wcard->prev && strncmp(temp, wcard->str, ft_strlen(wcard->str)))
@@ -34,15 +36,37 @@ static int	ms_ismatch(t_wcard *files, t_wcard *wcard)
 	return (0);
 }
 
+static int	ms_show_hidden(t_wcard *wcard)
+{
+	if (wcard->type == OP_STERM
+		&& wcard->str[0] == '.' && wcard->str[1] == '\0')
+		return (1);
+	return (0);
+}
+
+static int	ms_showall(t_wcard *wcard)
+{
+	if (wcard->type == OP_WCARD
+		&& wcard->next == NULL && wcard->prev == NULL)
+		return (1);
+	return (0);
+}
+
+static int	ms_ishidden(char *file)
+{
+	if (file[0] == '.')
+		return (1);
+	return (0);
+}
+
 int	ms_find_matches(t_wcard *wcard, t_wcard *files)
 {
 	t_wcard	*twcard;
 	int		match_all;
+	int		show_hidden;
 
-	match_all = 0;
-	if (wcard->type == OP_WCARD
-		&& wcard->next == NULL && wcard->prev == NULL)
-		match_all = 1;
+	match_all = ms_showall(wcard);
+	show_hidden = ms_show_hidden(wcard);
 	while (files)
 	{
 		twcard = wcard;
@@ -52,7 +76,8 @@ int	ms_find_matches(t_wcard *wcard, t_wcard *files)
 				twcard = twcard->next;
 			if (!twcard && !match_all)
 				break ;
-			if (match_all == 1 || ms_ismatch(files, twcard))
+			if ((match_all == 1 &&
+				!ms_ishidden(files->str)) || ms_ismatch(files, twcard, show_hidden))
 				files->ismatch = 1;
 			else
 			{
