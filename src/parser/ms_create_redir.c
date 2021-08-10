24d72ff10b8e9e07a545aa62a58c8864f785d8b7
Anthony <toni@Mac-mini-de-Anthony.local>
2021-08-10 18:39:28 +0200

    :bugs: fix >>

--- src/parser/ms_create_redir.c
+++ src/parser/ms_create_redir.c
@@ -6,7 +6,7 @@
 /*   By: sshakya <sshakya@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/04 22:52:40 by sshakya           #+#    #+#             */
-/*   Updated: 2021/08/04 22:53:16 by sshakya          ###   ########.fr       */
+/*   Updated: 2021/08/10 18:37:55 by atruphem         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,10 +67,13 @@ int	ms_redir_outa(t_tlist **token, t_command *command)
 		return (ERR_SYN);
 	command->redir_out = 1;
 	file = ms_format_file((*token)->next->tk.value);
-	command->in_fd = open(file, O_WRONLY | O_CREAT | O_APPEND, 0644);
+	command->out_fd = open(file, O_WRONLY | O_CREAT | O_APPEND, 0644);
 	free(file);
-	if (command->in_fd < 0)
-		return (ERR_OPEN);
+	if (command->out_fd < 0)
+	{	
+		command->error = errno;
+		command->error_file_name = (*token)->next->tk.value;
+	}
 	*token = (*token)->next->next;
 	return (0);
 }
